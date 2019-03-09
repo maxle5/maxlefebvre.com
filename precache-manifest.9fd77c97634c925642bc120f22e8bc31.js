09c960a9dcd69571431ff05d8c8b83faf28f989b
Max Lefebvre <max.lefebvre@garvin-allen.com>
2019-03-09 12:14:07 -0400

    Updates

--- precache-manifest.9fd77c97634c925642bc120f22e8bc31.js
+++ precache-manifest.9fd77c97634c925642bc120f22e8bc31.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/maxlefebvre.com/static/js/runtime~main.fbb0bdce.js"
   },
   {
-    "revision": "b2a9e74d0ce8767818b5",
-    "url": "/maxlefebvre.com/static/js/main.88c92932.chunk.js"
+    "revision": "1a9d4a9ab374b38c47ea",
+    "url": "/maxlefebvre.com/static/js/main.ceaaebb2.chunk.js"
   },
   {
     "revision": "ac164eeb7fe39897100f",
     "url": "/maxlefebvre.com/static/js/2.aa7a0768.chunk.js"
   },
   {
-    "revision": "b2a9e74d0ce8767818b5",
+    "revision": "1a9d4a9ab374b38c47ea",
     "url": "/maxlefebvre.com/static/css/main.c20b8033.chunk.css"
   },
   {
-    "revision": "a3bc6627d778cfc642b861d3e42dbebd",
+    "revision": "4222d4b9c219fe1fd05cf4aff46e94e3",
     "url": "/maxlefebvre.com/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/maxlefebvre.com/precache-manifest.4015431d0ca8bae6bee128a8b4040f3f.js"
+  "/maxlefebvre.com/precache-manifest.9fd77c97634c925642bc120f22e8bc31.js"
 );
 
 workbox.clientsClaim();
