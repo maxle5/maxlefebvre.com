b9d665fdd5b1fedd744f67e32af959a9ee0d6182
Max Lefebvre <max.lefebvre@garvin-allen.com>
2019-03-09 12:13:46 -0400

    adding gh-pages

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -4,7 +4,7 @@ export default function Header() {
     return (
         <header>
             <div className='container d-flex justify-content-between align-items-center'>
-                <img className='icon' src='/images/icon.png' />
+                <img className='icon' src={process.env.PUBLIC_URL + '/images/icon.png'} />
                 <div>
                     <a href='#about' className='active text-heading'>about</a>
                     <a href='#work' className='text-heading'>work</a>
--- src/components/sections/About.js
+++ src/components/sections/About.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function About() {
     return (
         <section id='about' className='container'>
-            <img className='icon d-block mx-auto' src='/images/icon.png' />
+            <img className='icon d-block mx-auto' src={process.env.PUBLIC_URL + '/images/icon.png'} />
             <h2 className='text-center text-primary'>{'{'} About me {'}'}</h2>
             <p className='mx-auto text-center'>
                 I'm a software developer from Halifax, NS with a passion for learning.
